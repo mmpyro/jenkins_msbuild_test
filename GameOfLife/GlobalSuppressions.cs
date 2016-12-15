6f1c4ac8682fd868042fba0b5fdb75ccef5b5ab0
Michal Marszalek <mmpyro@gmail.com>
2016-12-15 19:03:12 +0100

    ;

---
+++ GameOfLife/GlobalSuppressions.cs
@@ -0,0 +1,13 @@
+﻿
+// This file is used by Code Analysis to maintain SuppressMessage 
+// attributes that are applied to this project.
+// Project-level suppressions either have no target or are given 
+// a specific target and scoped to a namespace, type, member, etc.
+
+[assembly: System.Diagnostics.CodeAnalysis.SuppressMessage("Build", "S1854:Remove this useless assignment to local variable \"sum\".", Justification = "<Pending>", Scope = "member", Target = "~M:GameOfLife.Board.GetLiveNeighbours(System.Int32,System.Int32)~System.Int32")]
+[assembly: System.Diagnostics.CodeAnalysis.SuppressMessage("Build", "S1135:Complete the task associated to this \"TODO\" comment.", Justification = "<Pending>", Scope = "member", Target = "~M:GameOfLife.ConwaysRule.Perform(System.Boolean,System.Int32,System.Boolean@)")]
+[assembly: System.Diagnostics.CodeAnalysis.SuppressMessage("Build", "S1871:Either merge this branch with the identical one on line 22 or change one of the implementations.", Justification = "<Pending>", Scope = "member", Target = "~M:GameOfLife.ConwaysRule.Perform(System.Boolean,System.Int32,System.Boolean@)")]
+[assembly: System.Diagnostics.CodeAnalysis.SuppressMessage("Build", "S1125:Remove the unnecessary Boolean literal(s).", Justification = "<Pending>", Scope = "member", Target = "~M:GameOfLife.ConwaysRule.Perform(System.Boolean,System.Int32,System.Boolean@)")]
+[assembly: System.Diagnostics.CodeAnalysis.SuppressMessage("Build", "S2259:\"x\" is null on at least one execution path.", Justification = "<Pending>", Scope = "member", Target = "~M:GameOfLife.GoLObjectFactory.Create(GameOfLife.GoLPatterns)~GameOfLife.IGoLObject")]
+[assembly: System.Diagnostics.CodeAnalysis.SuppressMessage("Build", "S1481:Remove this unused \"result\" local variable.", Justification = "<Pending>", Scope = "member", Target = "~M:GameOfLife.GoLObjectFactory.Create(GameOfLife.GoLPatterns)~GameOfLife.IGoLObject")]
+[assembly: System.Diagnostics.CodeAnalysis.SuppressMessage("Build", "S1118:Add a \"protected\" constructor or the \"static\" keyword to the class declaration.", Justification = "<Pending>", Scope = "type", Target = "~T:GameOfLife.GoLObjectFactory")]
